9628d400ed103a32f2931cb96a93077acc42c6f2
treerock <treercock@myopera.com>
2013-11-15 14:15:09 +0000

    Switch to template

---
+++ project.php
@@ -0,0 +1,47 @@
+
+        <div class="row heading clearfix">
+            <h1>Treerock</h1>
+            <p class="subheading">
+                Sandbox Projects
+                <br />Learning
+                <br />\ experiments
+            </p>
+        </div>
+
+        <div class="row sb-project clearfix show">
+            <div class="col col-12" id="project">
+                <!-- enter project code here -->
+                <p id="status" class="highlight">File API &amp; FileReader API not supported</p>
+                <div id="holder" class="image drag"></div>
+                <p class="highlight instructions">Drag and drop an image file from your computer to the box above.</p>
+            </div>
+        </div>
+        <div class="row sb-switch clearfix">
+            <p>
+                Show Project Information
+            </p>
+        </div>
+        <div class="row sb-desc clearfix">
+            <div class="col col-6" id="meta">
+                <p>
+                    This is me playing around with the File Reader API. Inspired by
+                    <a href="http://jsfiddle.net/jimschubert/DqcUg/">this example on JSFiddle</a>. So cheers go to Jim Schubert and this
+                    <a href="http://stackoverflow.com/questions/11313414/html5-drag-and-drop-load-text-file-in-a-textbox-with-js">StackOverflow question</a>.
+                </p>
+                <p>
+
+                </p>
+            </div>
+            <div class="col col-6">
+                <p>June 2013</p>
+                <p>javascript, html5, api, window, File, FileReader</p>
+                <ul>
+                    <li><a href="http://www.html5rocks.com/en/tutorials/file/dndfiles/">Using the File APIs</a>
+                    </li>
+                    <li><a href="http://www.w3.org/TR/file-upload/">W3C File Specification</a>
+                    </li>
+                </ul>
+
+
+            </div>
+        </div>
