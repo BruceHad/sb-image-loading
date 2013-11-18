b8cd26439338d84ac692398aa5203faf8a13476c
treerock <treerock@myopera.com>
2013-11-18 14:06:25 +0000

    Cleaned up project page.

--- project.php
+++ project.php
@@ -9,28 +9,25 @@
 
         <div class="row sb-project clearfix show">
             <div class="col col-8">
-                <div id="holder">
-                    <p id="fr-status" class="fr-status">File API &amp; FileReader API not supported so this won't work.</p>
-                </div>
+                <p id="fr-status" class="fr-status">File API &amp; FileReader API not supported so this won't work.</p>
+                <div id="holder"></div>
             </div>
             <div class="col col-4 meta">
                 <h2>About</h2>
-                <p>
-                    This is me playing around with the File Reader API. Idea taken from 
-                    <a href="http://jsfiddle.net/jimschubert/DqcUg/">this example on JSFiddle</a>.
-                </p>
-                <div class="further hidden">
-                    <p>The file reader api...</p>
-                    <ul>
-                        <li><a href="http://www.html5rocks.com/en/tutorials/file/dndfiles/">Using the File APIs</a>
-                        </li>
-                        <li><a href="http://www.w3.org/TR/file-upload/">W3C File Specification</a>
-                        </li>
-                    </ul>
-                </div>
+                <p>This is me playing around with the File Reader API. Idea taken from 
+                    <a href="http://jsfiddle.net/jimschubert/DqcUg/">this example on JSFiddle</a>.</p>
+                <p>It uses HTML5 Drag and Drop<sup>[3]</sup> features that allow you to grab local files and drag them onto a webpage. The ondrop event handler fires when a file is 'dropped' on top of the #holder box. The dragged file can then be obtained from the FileList object.</p>
+                <p>Then the FileReader API<sup>[4]</sup> takes the file and users the readAsDataUrl method to creat a data: URL representing the file's data.</p>
+                <p>To display the image just update the box background with this image data url.</p>
+                <ul class="reading-list">
+                    <li>[1]: <a href="//www.html5rocks.com/en/tutorials/file/dndfiles/">Using the File APIs</a>
+                    </li>
+                    <li>[2]: <a href="//www.w3.org/TR/file-upload/">W3C File Specification</a></li>
+                    <li>[3]: <a href="//developer.mozilla.org/en-US/docs/DragDrop/Drag_and_Drop">Drag and Drop</a></li>
+                    <li>[4]: <a href="//developer.mozilla.org/en-US/docs/Web/API/FileReader">FileReader API</a></li>
+                </ul>
                 <h2>Meta</h2>
-                <p>Developed around June 2013</p>
-                <p> javascript, html5, api, window, File, FileReader</p>
+                <p>Created June 2013</p>
             </div>
         </div>
         <div class="row sb-desc clearfix">
--- scripts/project.js
+++ scripts/project.js
@@ -1,33 +1,26 @@
 var holder = document.getElementById('holder'),
-    state = document.getElementById('fr-status');
-
+    state = document.getElementById('fr-status'),
+    img_string = '';
 if(typeof window.FileReader === 'undefined') {
     state.className += ' fail';
 } else {
     state.className += ' success';
-    state.innerHTML = '<p>Cool. File API &amp; FileReader are available so this should work.</p>';
-    state.innerHTML += '<p>Drag and drop an image file from your computer to this box.</p>';
+    state.innerHTML = '<p>Cool. File API &amp; FileReader are available so this should work. Drag and drop an image file from your computer to this box.</p>';
 }
 
-holder.ondragover = function() {
-    this.className += ' hover';
+holder.ondragover = function(e) {
     return false;
 };
 
-holder.ondragend = function() {
-    this.className = '';
-    return false;
-};
 holder.ondrop = function(e) {
-    this.className = '';
+    state.innerHTML = '<p>Nice one! Did that work?</p>';
     e.preventDefault();
-    var file = e.dataTransfer.files[0],
-        reader = new FileReader();
+    
+    var file = e.dataTransfer.files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
     reader.onload = function(event) {
-        console.log(event.target);
         holder.style.background = 'url(' + event.target.result + ') no-repeat center';
     };
-    console.log(file);
-    reader.readAsDataURL(file);
     return false;
 };
